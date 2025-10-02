5d24b17866f14773a32bf8112658de9e33e2478c
falconstore <53540785+falconstore@users.noreply.github.com>
2025-10-02 14:26:15 -0300

    Delete components directory

--- components/CalculatorFreeProWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { CalculatorFreeProDirect } from "./CalculatorFreeProDirect";
-
-export const CalculatorFreeProWrapper = () => {
-  return (
-    <div className="w-full">
-      <CalculatorFreeProDirect />
-    </div>
-  );
-};
